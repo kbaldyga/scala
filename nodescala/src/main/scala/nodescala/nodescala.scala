fb9b606259dad0639baed2cf007a0012fc82124a
Kamil Bałdyga <kbaldyga@gmail.com>
2013-11-26 19:48:48 +0000

    i don't understand any of this...

--- nodescala/src/main/scala/nodescala/nodescala.scala
+++ nodescala/src/main/scala/nodescala/nodescala.scala
@@ -29,7 +29,12 @@ trait NodeScala {
    *  @param token        the cancellation token for
    *  @param body         the response to write back
    */
-  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = ???
+  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = {
+    for(s <- response) {
+      if(token.isCancelled) return;
+      exchange.write(s)
+    }
+  }
 
   /** A server:
    *  1) creates and starts an http listener
@@ -41,8 +46,17 @@ trait NodeScala {
    *  @param handler        a function mapping a request to a response
    *  @return               a subscription that can stop the server and all its asynchronous operations *entirely*.
    */
-  def start(relativePath: String)(handler: Request => Response): Subscription = ???
-
+  def start(relativePath: String)(handler: Request => Response): Subscription = {
+    val listener = createListener(relativePath)
+    listener.start
+    Future.run()(token => future {
+	  while(!token.isCancelled) {
+		val (req, exchange) = Await.result(listener.nextRequest, 100 millis)
+		respond(exchange, token, handler(req))
+	  }  
+    }
+    );
+  }
 }
 
 
@@ -111,7 +125,18 @@ object NodeScala {
      *  @param relativePath    the relative path on which we want to listen to requests
      *  @return                the promise holding the pair of a request and an exchange object
      */
-    def nextRequest(): Future[(Request, Exchange)] = ???
+    def nextRequest(): Future[(Request, Exchange)] = {
+      val promise = Promise[(Request, Exchange)]()
+      createContext(exchange =>
+      	Future {
+      	  (exchange.request, exchange)
+      	} onComplete {
+      	  removeContext 
+      	  promise.complete
+      	}
+      )
+      promise.future
+    }
   }
 
   object Listener {
